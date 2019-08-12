4298e6995e51b68923a29208675d4a9cebd02e83
Prakash <jyoti.prakash@cardinalhealth.com>
2019-08-12 14:32:00 -0400

    update

--- dockerimages.sh
+++ dockerimages.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -e
-docker build --tag="jyoprakash/jmeter-base:5.0" -f Dockerfile-base .
 docker build --tag="jyoprakash/jmeter-master:5.0" -f Dockerfile-master .
 docker build --tag="jyoprakash/jmeter-slave:5.0" -f Dockerfile-slave .
 
